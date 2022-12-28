1a4cd572d5b1eb9c90896710d945188e9b6d9c04
John <108818816+johncuysal@users.noreply.github.com>
2022-12-28 01:03:40 -0500

    Added integer and rational number pages.

--- scripts/script.js
+++ scripts/script.js
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
         // Make an HTTP request to retrieve root/journal/index.html's contents.
-        fetch('')
+        fetch('/journal/index.html')
             .then(response => response.text())
             .then(html => {
                 // Parse the HTML into a DOM tree
@@ -25,7 +25,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 const doc = parser.parseFromString(html, 'text/html');
 
                 // Search for elements whose inner html includes the search term
-                const matchingElements = Array.from(doc.querySelectorAll('h2'))
+                const matchingElements = Array.from(doc.querySelectorAll('.journal-link'))
                     .filter(element => element.innerHTML.toLowerCase().includes(searchTerm));
 
                 // Clear the search results container
@@ -36,9 +36,28 @@ window.addEventListener('DOMContentLoaded', () => {
                     let searchResult = document.createElement('a')
                     searchResult.innerHTML = element.innerHTML;
                     searchResult.classList.add('search-result')
-                    searchResult.setAttribute('href', element.innerHTML.toLowerCase());
+                    searchResult.setAttribute('href', '../' + element.getAttribute('href'));
                     searchResults.appendChild(searchResult);
                 });
+
+                searchResults.style.display = 'flex';
             });
+    });
+
+    // Add an event listener to the search field that listens for the "blur" event
+    searchField.addEventListener('blur', () => {
+        // When the search field loses focus, set the display style of the search results element to "none"
+        searchResults.style.display = 'none';
+    });
+
+    // Add an event listener to the search field that listens for the "focus" event
+    searchField.addEventListener('focus', () => {
+        // When the search field gains focus, set the display style of the search results element to "flex"
+        searchResults.style.display = 'flex';
+    });
+
+    searchResults.addEventListener('mousedown', event => {
+        // Prevent the default action of the event (which would cause the search field to lose focus)
+        event.preventDefault();
     });
 });
