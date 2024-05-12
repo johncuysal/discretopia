5703699496949f6df75fc77881a0a6e1039ebc1e
John <108818816+johncuysal@users.noreply.github.com>
2024-05-12 17:54:26 -0400

    completed project

--- README.md
+++ README.md
@@ -8,20 +8,25 @@ The friendly discrete math reference.
 
 ## About the project
 
-Discretopia (https://discretopia.com) is an educational website that acts as a unified, organized, and friendly guide to the essential topics covered in a college-level discrete mathematics course.
+Discretopia (https://discretopia.com) is an educational website that acts as a unified, organized, and friendly guide to
+the essential topics covered in a college-level discrete mathematics course.
 
-With a highly-linked structure inspired by Wikipedia, related concepts are no more than a few clicks away. In addition to making it easier to review previously covered topics as needed, this level of interconnectedness also provides readers the opportunity to go down a discrete math rabbit hole and satisfy their curiosity.
+With a highly-linked structure inspired by Wikipedia, related concepts are no more than a few clicks away. This level of
+interconnectedness provides readers the opportunity to go down a discrete math rabbit hole and satisfy their curiosity.
 
 ## Key features
 
-- Well-written, thoughtful entries on 100+ discrete math topics, including: logic, proofs, set theory, functions, algorithms, relations, number theory, and induction.
-- A smart search bar that recognizes alternative names for topics in discrete math and efficiently redirects readers to the correct entry.
+- Well-written, thoughtful entries on 130+ discrete math topics, including: logic, proofs, set theory, functions,
+  algorithms, relations, number theory, induction, combinatorics, graph theory, and recursion.
+- A smart search bar that recognizes alternative names for topics in discrete math and efficiently redirects readers to
+  the correct entry.
 - Excellent internal linking, making it easy to jump from entry to entry.
 - A beautiful design, which has a profound effect on learning.
 - Built to be as accessible and approachable as possible.
 
 ## License
 
-© 2024 John C. Uysal. All Rights Reserved.
+© 2022 — 2024 John C. Uysal. All Rights Reserved.
 
-This project is fully protected by copyright law. No part of this project, including but not limited to code, content, and design, may be reproduced, modified, or distributed without explicit permission from the project creator.
+This project is fully protected by copyright law. No part of this project, including but not limited to code, content,
+and design, may be reproduced, modified, or distributed without explicit permission from the project creator.
--- scripts/script.js
+++ scripts/script.js
@@ -1,118 +1,179 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
     // Counting journal entries
-    const journalLinkContainers = document.querySelectorAll(".journal-link-container");
+    const journalBlocks = document.querySelectorAll(".journal-block");
+    const journalStatisticTotalEntries = document.querySelector("#journal-statistic-total-entries");
     let numEntries = 0;
-    journalLinkContainers.forEach(journalLinkContainer => {
-        const links = journalLinkContainer.querySelectorAll('.journal-link');
-        const filteredLinks = Array.from(links).filter(link => !link.classList.contains('search-keyword'));
+    journalBlocks.forEach(journalBlock => {
+        const links = journalBlock.querySelectorAll(".journal-block-link");
+        const filteredLinks = Array.from(links).filter(link => !link.classList.contains("search-keyword"));
         const linkCount = filteredLinks.length;
-        const associatedH2 = journalLinkContainer.previousElementSibling.previousElementSibling;
-        //associatedH2.textContent = associatedH2.textContent + ` (${linkCount} entries)`;
-        numEntries = numEntries + linkCount;
+        const associatedH2 = journalBlock.querySelector(".journal-block-title-statistic");
+        associatedH2.textContent = `${linkCount} entries`;
+        numEntries += linkCount;
     });
-    if (numEntries !== 0) {
-        document.querySelector("h1").textContent = document.querySelector("h1").textContent + ` (${numEntries} entries)`;
+    if (journalStatisticTotalEntries) {
+        journalStatisticTotalEntries.textContent = `${numEntries} entries`;
     }
 
-    // Done counting journal entries
-
-    // Get the table of contents div
+    // Populate the list of contents
     const listOfContents = document.querySelector("#list-of-contents");
-
-    // Get all the h2 elements on the page
-    const h2Elements = document.querySelectorAll("h2");
-
-    // Loop through the h2 elements
-    for (const h2 of h2Elements) {
-        // Set the h2 element's id attribute to the modified inner HTML
-        h2.setAttribute("id", h2.innerHTML.toLowerCase().replace(/ /g, "-").replace('&amp;', 'and').replace("'", ''));
-
-        if (listOfContents) {
-            // Create a new anchor element
-            const a = document.createElement("a");
-            // Set the href attribute to the h2 element's text content
-            a.setAttribute("href", "#" + h2.id);
-            a.classList.add('list-of-contents-link')
-            // Set the anchor element's text content to the h2 element's text content
-            a.innerHTML = h2.innerHTML;
-
-            // Append the anchor element to the table of contents div
-            listOfContents.appendChild(a);
+    if (listOfContents) {
+        const h2Elements = document.querySelectorAll("h2");
+        for (const h2 of h2Elements) {
+            h2.setAttribute("id", h2.innerHTML.toLowerCase().replace(/ /g, "-").replace("&amp;", "and").replace("'", ""));
+            if (listOfContents) {
+                const a = document.createElement("a");
+                a.setAttribute("href", "#" + h2.id);
+                a.classList.add("list-of-contents-link");
+                a.innerHTML = h2.innerHTML;
+                listOfContents.appendChild(a);
+            }
         }
     }
 
-    // Deal with td elements for truth tables and give them appropriate background colors.
-    const tdElements = document.querySelectorAll('td');
+    // Setting current entry to bold in entry navbox, giving links to previous and next buttons
+    const entryNavboxContainer = document.querySelector("#entry-navbox-container");
+    if (entryNavboxContainer) {
+        const entryNavLinks = entryNavboxContainer.querySelectorAll("a");
+        const entryTitle = document.querySelector("h1");
+        for (let i = 0; i < entryNavLinks.length; i++) {
+            const currentEntryNavLink = entryNavLinks[i];
+            if (currentEntryNavLink.textContent === entryTitle.textContent) {
+                currentEntryNavLink.classList.add("current-entry-navlink");
+                currentEntryNavLink.removeAttribute("href");
+                const previousEntryNavLink = i > 0 ? entryNavLinks[i - 1] : entryNavLinks[entryNavLinks.length - 1];
+                const nextEntryNavLink = i < entryNavLinks.length - 1 ? entryNavLinks[i + 1] : entryNavLinks[0];
+                if (previousEntryNavLink) {
+                    const previousButtons = document.querySelectorAll(".preventry-button");
+                    previousButtons.forEach(previousButton => {
+                        previousButton.setAttribute("href", previousEntryNavLink.getAttribute("href"));
+                        const previousButtonDescription = previousButton.querySelector(".entry-button-description");
+                        previousButtonDescription.textContent = previousEntryNavLink.textContent;
+                    });
+                }
+                if (nextEntryNavLink) {
+                    const nextButtons = document.querySelectorAll(".nextentry-button");
+                    nextButtons.forEach(nextButton => {
+                        nextButton.setAttribute("href", nextEntryNavLink.getAttribute("href"));
+                        const nextButtonDescription = nextButton.querySelector(".entry-button-description");
+                        nextButtonDescription.textContent = nextEntryNavLink.textContent;
+                    });
+                }
+                break;
+            }
+        }
+    }
 
+    // Colorize truth tables
+    const tdElements = document.querySelectorAll("td");
     tdElements.forEach(td => {
-        if (td.innerHTML === '1') {
-            td.style.backgroundColor = '#9fe0a5';
-        } else if (td.innerHTML === '0') {
-            td.style.backgroundColor = '#e0a59f';
+        if (td.innerHTML === "1") {
+            td.style.backgroundColor = "#9fe0a5";
+        } else if (td.innerHTML === "0") {
+            td.style.backgroundColor = "#e0a59f";
         }
     });
 
-    // Get a reference to the search field and search results
-    const searchField = document.getElementById('search-field');
-    const searchResults = document.getElementById('search-results');
+    /**
+     * Toggle mobile menu dropdown visibility
+     */
+    const mobileMenuButton = document.getElementById("mobile-menu");
+    const mobileMenuDropdown = document.getElementById("mobile-menu-dropdown");
+    let isInMobileMode;
+    isInMobileMode = window.innerWidth < 1080;
+    mobileMenuDropdown.classList.add("hidden");
+    mobileMenuButton.addEventListener("click", () => {
+        mobileMenuDropdown.classList.toggle("hidden");
+        if (mobileMenuButton.style.backgroundColor !== "var(--hyperlink-transparent-color)") {
+            mobileMenuButton.style.backgroundColor = "var(--hyperlink-transparent-color)";
+        } else {
+            mobileMenuButton.style.backgroundColor = "var(--body-background-color)";
+        }
+    });
+    window.addEventListener("resize", () => {
+        if (isInMobileMode && window.innerWidth > 1080) { // Exiting mobile mode
+            if (!mobileMenuDropdown.classList.contains("hidden")) {
+                mobileMenuDropdown.classList.add("hidden");
+                mobileMenuButton.style.backgroundColor = "var(--body-background-color)";
+            }
+            isInMobileMode = false;
+        } else if (!isInMobileMode && window.innerWidth <= 1080) { // Entering mobile mode
+            isInMobileMode = true;
+        }
+    });
+    mobileMenuButton.addEventListener("blur", () => {
+        if (!mobileMenuDropdown.classList.contains("hidden")) {
+            mobileMenuDropdown.classList.add("hidden");
+            mobileMenuButton.style.backgroundColor = "var(--body-background-color)";
+        }
+    });
+    // When the mobile menu dropdown get clicked, prevent it from losing focus (overrides default behavior)
+    mobileMenuDropdown.addEventListener("mousedown", event => {
+        event.preventDefault();
+    });
 
-    // Listen for changes in the search field input
-    searchField.addEventListener('input', () => {
-        // Extract the currently entered search term
+    /**
+     * Searching functionality
+     */
+    const searchField = document.getElementById("search-field");
+    const searchResults = document.getElementById("search-results");
+    searchField.addEventListener("input", () => {
         const searchTerm = searchField.value.toLowerCase();
-
-        // Check if the search term is empty
-        if (searchTerm.trim() === '') {
-            // Clear the search results if the search term is empty, and return
-            searchResults.innerHTML = '';
+        if (searchTerm.trim() === "") {
+            searchResults.innerHTML = ""; // Clear the search results and return if whitespace was entered
             return;
         }
-
-        // Make an HTTP request to retrieve root/journal/index.html's contents.
-        fetch('/journal/index.html')
-            .then(response => response.text())
-            .then(html => {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(html, 'text/html');
-
-                // Search for elements whose inner html includes the search term
-                const matchingElements = Array.from(doc.querySelectorAll('.journal-link'))
-                    .filter(element => element.innerHTML.toLowerCase().includes(searchTerm));
-
-                // Clear the search results container
-                searchResults.innerHTML = '';
-
-                // Display the matching elements in the search results container
-                let numResultsAdded = 0;
-                matchingElements.forEach(element => {
-                    if (numResultsAdded < 10) {
-                        let searchResult = document.createElement('a')
-                        searchResult.innerHTML = element.innerHTML;
-                        searchResult.classList.add('search-result')
-                        searchResult.setAttribute('href', element.getAttribute('href'));
-                        searchResults.appendChild(searchResult);
-                        numResultsAdded++;
-                    }
-                });
-
-                searchResults.style.display = 'flex';
+        // HTTP request to retrieve the contents of /journal/index.html
+        fetch("/journal/index.html").then(response => response.text()).then(html => {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(html, "text/html");
+            // Search for elements whose inner html includes the search term
+            const matchingElements = Array.from(doc.querySelectorAll(".journal-block-link"))
+                .filter(element => element.innerHTML.toLowerCase().includes(searchTerm));
+            // Clear the search results container
+            searchResults.innerHTML = "";
+            // Display the matching elements in the search results container
+            let numResultsAdded = 0;
+            matchingElements.forEach(element => {
+                if (numResultsAdded < 10) {
+                    let searchResult = document.createElement("a")
+                    searchResult.innerHTML = element.innerHTML;
+                    searchResult.classList.add("search-result")
+                    searchResult.setAttribute("href", element.getAttribute("href"));
+                    searchResults.appendChild(searchResult);
+                    numResultsAdded++;
+                }
             });
+            searchResults.style.display = "flex";
+        });
     });
-
-    // Add an event listener to the search field that listens for the "blur" event
-    searchField.addEventListener('blur', () => {
-        // When the search field loses focus, set the display style of the search results element to "none"
-        searchResults.style.display = 'none';
+    // When the enter key is pressed in the search field, redirect to the first search result
+    searchField.addEventListener("keydown", event => {
+        if (event.key === "Enter") {
+            const firstSearchResult = searchResults.querySelector(".search-result");
+            if (firstSearchResult) {
+                window.location.href = firstSearchResult.getAttribute("href");
+            }
+        }
     });
-
-    // Add an event listener to the search field that listens for the "focus" event
-    searchField.addEventListener('focus', () => {
-        // When the search field gains focus, set the display style of the search results element to "flex"
-        searchResults.style.display = 'flex';
+    // Also, redirect to the first search result if the go button is clicked
+    const goButton = document.getElementById("go-button");
+    goButton.addEventListener("click", () => {
+        const firstSearchResult = searchResults.querySelector(".search-result");
+        if (firstSearchResult) {
+            window.location.href = firstSearchResult.getAttribute("href");
+        }
     });
-
-    searchResults.addEventListener('mousedown', event => {
-        // Prevent the default action of the event (which would cause the search field to lose focus)
+    // When the search field loses focus, hide the search results
+    searchField.addEventListener("blur", () => {
+        searchResults.style.display = "none";
+    });
+    // When the search field gains focus, show the search results
+    searchField.addEventListener("focus", () => {
+        searchResults.style.display = "flex";
+    });
+    // When the search results get clicked, prevent the search field from losing focus (overrides default behavior)
+    searchResults.addEventListener("mousedown", event => {
         event.preventDefault();
     });
 });
